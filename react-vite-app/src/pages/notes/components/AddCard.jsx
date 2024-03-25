e6694164e3c92f4b49bc3b063a099033ae91af92
Harsh <hgaur491@gmail.com>
2024-03-25 17:05:31 +0530

    ui changes

--- react-vite-app/src/pages/notes/components/AddCard.jsx
+++ react-vite-app/src/pages/notes/components/AddCard.jsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 export default function AddCard(props) {
+    const [adding,setAdding]=useState();
     const { handleAddCard ,type} = props;
-    const {register,handleSubmit} = useForm({})
+    const {register,handleSubmit,formState:{errors}} = useForm({})
     const onSubmit=({title})=>{
         handleAddCard({
             title:title,
@@ -11,10 +13,30 @@ export default function AddCard(props) {
     }
     return (
         <div>
+            {adding?
             <form onSubmit={handleSubmit(onSubmit)}>
-                <input type="text" {...register("title")} />
-                <button>add</button>
+                <textarea className="w-full bg-slate-800 p-2 rounded-md"
+                 type="text" {...register("title",{
+                    required: {
+                        value:true,
+                        message:"input could not be empty"
+                    }
+                })} />
+                <p className="text-xs text-red-700 font-medium">{errors.title&& errors.title.message}</p>
+                <div className="flex justify-end gap-2 text-xs">
+                    <button onClick={()=>setAdding(false)} type="button">Cancel</button>
+                    <button>add</button>
+                </div>
             </form>
+            
+            :
+            <button
+                className="w-full p-2 border-2 border-neutral-500/20 bg-neutral-800 shadow-sm shadow-neutral-500/20 rounded-md"
+                onClick={()=>setAdding(true)}
+                >
+                Add Note
+            </button>
+            }
         </div>
     );
 }
